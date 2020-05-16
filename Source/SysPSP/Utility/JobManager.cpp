1b5553632ff29ef4584e1233c41721544d550cd8
= <catapultit@me.com>
2020-05-16 22:06:19 +1000

    Ensure all Utility  libs are fully qualified.

--- Source/SysPSP/Utility/JobManager.cpp
+++ Source/SysPSP/Utility/JobManager.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 
 #include "stdafx.h"
-#include "JobManager.h"
+#include "SysPSP/Utility/JobManager.h"
 
 #include <string.h>
 #include <stdio.h>
--- Source/Utility/Translate.cpp
+++ Source/Utility/Translate.cpp
@@ -26,8 +26,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <string>
 
 #include "System/IO.h"
-#include "StringUtil.h"
-#include "VolatileMem.h"
+#include "Utility/StringUtil.h"
+#include "Utility/VolatileMem.h"
 
 #include "System/Paths.h"
 #include "SysPSP/Utility/PathsPSP.h"
