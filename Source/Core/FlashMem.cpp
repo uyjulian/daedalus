efc255de772cac78db28003f3939a1910d7f2ede
= <catapultit@me.com>
2020-05-21 12:57:08 +1000

    Add a header for FlashMem

--- Source/Core/FlashMem.cpp
+++ Source/Core/FlashMem.cpp
@@ -18,6 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "Base/Daedalus.h"
+#include "Core/FlashMem.h"
 #include "Debug/DBGConsole.h"
 #include "Memory.h"
 #include "DMA.h"
