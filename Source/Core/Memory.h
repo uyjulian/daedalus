6779af67f3cedab6c4a179ee588de2476790e683
Wally4000 <catapultit@me.com>
2020-05-23 23:15:08 +1000

    Make QuickRead functions const correct / void pointers

--- Source/Core/Memory.h
+++ Source/Core/Memory.h
@@ -139,47 +139,44 @@ bool Memory_GetInternalReadAddress(u32 address, const void ** p_translated);
 #endif
 
 
-//////////////////////////////////////////////////////////////
 // Quick Read/Write methods that require a base returned by
 // ReadAddress or Memory_GetInternalReadAddress etc
 
-inline u64 QuickRead64Bits( u8 *p_base, u32 offset )
+inline u64 QuickRead64Bits(const void* ptr, u32 offset)
 {
-	u64 data = *(u64 *)(p_base + offset);
-	return (data>>32) + (data<<32);
+	u64 data = *(u64*)(static_cast<const u8*>(ptr) + offset);
+	return (data >> 32) + (data << 32);
 }
 
-inline u32 QuickRead32Bits( u8 *p_base, u32 offset )
+inline u32 QuickRead32Bits(const void* ptr, u32 offset)
 {
-	return *(u32 *)(p_base + offset);
+	return *(u32*)(static_cast<const u8*>(ptr) + offset);
 }
 
-inline u16 QuickRead16Bits( u8 *p_base, u32 offset )
+inline u16 QuickRead16Bits(const void* ptr, u32 offset)
 {
-	return *(u16 *)((uintptr_t)(p_base + offset) ^ U16_TWIDDLE);
+	return *(u16*)((uintptr_t)(static_cast<const u8*>(ptr) + offset) ^ U16_TWIDDLE);
 }
 
-inline void QuickWrite16Bits( u8 *p_base, u32 offset, u16 value)
+inline void QuickWrite16Bits(void* ptr, u32 offset, u16 value)
 {
-	*(u16 *)((uintptr_t)(p_base + offset) ^ U16_TWIDDLE) = value;
+	*(u16*)((uintptr_t)(static_cast<u8*>(ptr) + offset) ^ U16_TWIDDLE) = value;
 }
 
-inline void QuickWrite64Bits( u8 *p_base, u32 offset, u64 value )
+inline void QuickWrite64Bits(void* ptr, u32 offset, u64 value)
 {
-	u64 data = (value>>32) + (value<<32);
-	*(u64 *)(p_base + offset) = data;
+	*(u64*)(static_cast<u8*>(ptr) + offset) = (value >> 32) + (value << 32);
 }
 
-inline void QuickWrite32Bits( u8 *p_base, u32 offset, u32 value )
+inline void QuickWrite32Bits(void* ptr, u32 offset, u32 value)
 {
-	*(u32 *)(p_base + offset) = value;
+	*(u32*)(static_cast<u8*>(ptr) + offset) = value;
 }
 
-inline void QuickWrite32Bits( u8 *p_base, u32 value )
+inline void QuickWrite32Bits(const void* ptr, u32 value)
 {
-	*(u32 *)(p_base) = value;
+	*(u32*)ptr = value;
 }
-
 
 #define MEMORY_SIZE_RDRAM				0x400000
 #define MEMORY_SIZE_EXRDRAM				0x400000
