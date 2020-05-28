2f34f54782f39a732af98b872035b1ac2eb68717
= <catapultit@me.com>
2020-05-28 20:24:56 +1000

    Move __PRETTY_FUNCTION__ and _DO_NOT_DECLARE_INTERLOCKED_INTRINSICS_IN_MEMORY out of Platform.h

--- Source/Base/Macros.h
+++ Source/Base/Macros.h
@@ -49,6 +49,15 @@
 #define DAEDALUS_HALT __builtin_trap()
 #endif
 
+
+// Function name introspection.
+#ifdef _MSC_VER
+#define DAEDALUS_FUNCTION_NAME __FUNCTION__
+#else
+#define DAEDALUS_FUNCTION_NAME __PRETTY_FUNCTION__
+#endif
+
+
 #ifdef DAEDALUS_DEBUG_CONSOLE
 #define NODEFAULT		DAEDALUS_ERROR( "No default - we shouldn't be here" )
 #endif
