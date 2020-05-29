4f99b90c4807a494b3b9fdfc6f083b1a9598493a
= <catapultit@me.com>
2020-05-29 16:21:09 +1000

    Delete platform/BuildOptions.h and select buildconfig.h directly from stdafx.h. Delete Stdafx.cpp

--- Source/stdafx.h
+++ Source/stdafx.h
@@ -22,10 +22,38 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef STDAFX_H_
 #define STDAFX_H_
 
-//*****************************************************************************
-//	These are our various compilation options
-//*****************************************************************************
-#include "Platform/BuildOptions.h"
+
+#ifndef MAKE_UNCACHED_PTR
+#ifdef DAEDALUS_PSP
+#define MAKE_UNCACHED_PTR(x)	(reinterpret_cast< void * >( reinterpret_cast<u32>( (x) ) | 0x40000000 ))
+#else
+#define MAKE_UNCACHED_PTR(x)	(x)
+#endif
+#endif
+
+// Pure is a function attribute which says that a function does not modify any global memory.
+// Const is a function attribute which says that a function does not read/modify any global memory.
+
+// Given that information, the compiler can do some additional optimisations.
+
+#ifndef DAEDALUS_ATTRIBUTE_PURE
+#define DAEDALUS_ATTRIBUTE_PURE
+#endif
+
+#ifndef DAEDALUS_ATTRIBUTE_CONST
+#define DAEDALUS_ATTRIBUTE_CONST
+#endif
+
+#if defined(DAEDALUS_CONFIG_RELEASE)
+#include "Base/Release/BuildConfig.h"
+#elif defined(DAEDALUS_CONFIG_PROFILE)
+#include "Base/Profile/BuildConfig.h"
+#elif defined(DAEDALUS_CONFIG_DEV)
+#include "Base/Dev/BuildConfig.h"
+#else
+#error Unknown compilation mode
+#endif
+
 
 // Platform specifc #includes, externs, #defines etc
 #ifdef DAEDALUS_W32
